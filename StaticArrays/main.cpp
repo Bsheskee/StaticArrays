1c0b3624c94b7a80ed853060d5c1a24968e62e04
Bsheskee <bialobrzewski.business@gmail.com>
2025-03-08 18:34:56 +0100

    Initial Commit

---
+++ StaticArrays/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StaticArrays
+//
+//  Created by Bartłomiej Białobrzewski on 08/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
